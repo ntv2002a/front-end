cfd12e8d98d8f5a5c6176275dead3666cbf873a7
Nguyen Trung Vu <trungvu16502@gmail.com>
2023-02-27 15:54:10 +0700

    Connect Wallet

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -3,23 +3,34 @@ import logo from '../../Aura-logo-6.png';
 import '../../App.css';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar, Container, Button } from 'react-bootstrap';
+import { WalletConnection } from "../Pages/HomePage/WalletConnection";
+import { SigningStargateClient } from "@cosmjs/stargate";
+import { AccountData } from "@keplr-wallet/types";
 
 export const NavBar = () => {
-    return (
-        <div className="App-navbar">
-          <Navbar bg="dark" variant="dark">
-            <Container>
-              <Navbar.Brand href="">
-                <img src={logo} className="App-logo" alt="logo" />
-              </Navbar.Brand>
-              <Nav className="me-auto">
-                <Nav.Link href="./">Home</Nav.Link>
-                <Nav.Link href="./dashboard">Dashboard</Nav.Link>
-              </Nav>
-              <Nav><Button size='lg' variant="outline-light">Connect Wallet</Button></Nav>
-            </Container>
-          </Navbar>
-          <br />
-        </div>
-      );
+  let [signingClient, setSigningClient] = React.useState<SigningStargateClient|null>(null);
+  let [account, setAccount] = React.useState<AccountData|null>(null);
+  let [balance, setBalance] = React.useState<number>(NaN);
+  
+  const functionSignOut = () => setSigningClient(null);
+
+  return (
+    <div className="App-navbar">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="">
+            <img src={logo} className="App-logo" alt="logo" />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="./">Home</Nav.Link>
+            <Nav.Link href="./dashboard">Dashboard</Nav.Link>
+          </Nav>
+          <Nav>
+              <WalletConnection setSigningClient = {setSigningClient} setAccount = {setAccount} setBalance = {setBalance}/>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+    </div>
+  );
 }
---
+++ src/components/Pages/HomePage/WalletConnection.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { ChainInfo, Window as KeplrWindow } from "@keplr-wallet/types";
+import { AccountData, OfflineSigner } from "@cosmjs/proto-signing"
+import { Account, Coin, SigningStargateClient, StargateClient } from "@cosmjs/stargate";
+import {Button } from 'react-bootstrap';
+
+declare global {
+    interface Window extends KeplrWindow { }
+}
+
+const chainId: string = 'enphoria-2';
+const rpcUlr: string = 'https://rpc.euphoria.aura.network';
+
+interface WalletConnectionProps {
+    setSigningClient: React.Dispatch<React.SetStateAction<any>>,
+    setAccount: React.Dispatch<React.SetStateAction<any>>,
+    setBalance: React.Dispatch<React.SetStateAction<number>>
+}
+
+export function WalletConnection({setSigningClient, setAccount, setBalance} : WalletConnectionProps) {
+    const connectKeplr = async () => {
+        const { keplr } = window;
+
+        if (!keplr) {
+            alert('Your browser has not been installed Keplr Wallet extension!');
+            return;
+        }
+        await keplr.experimentalSuggestChain(getTestnetChainInfo());
+
+        const offlineSigner: OfflineSigner = window.getOfflineSigner!(chainId);
+        const signingClient : StargateClient = await SigningStargateClient.connectWithSigner(
+            rpcUlr,
+            offlineSigner
+        );
+        const account: AccountData = (await offlineSigner.getAccounts())[0];
+        const address: string = account.address;
+        
+        if (address != undefined) {
+            const balanceAsCoin: Coin = await signingClient.getBalance(address, 'ueaura');
+            const balance : number = parseFloat(balanceAsCoin.amount) * 1/1000000;
+
+            setSigningClient(signingClient);
+            setAccount(account);
+            setBalance(balance);
+        };
+    }
+    return (
+        <div>
+            <Button onClick={connectKeplr} size='lg' variant="outline-light">
+              Connect Waller
+            </Button>
+        </div>
+    );    
+}
+
+const getTestnetChainInfo = (): ChainInfo => ({
+    "chainId": "euphoria-2",
+    "chainName": "Aura Euphoria Testnet",
+    "rpc": "https://rpc.euphoria.aura.network",
+    "rest": "https://lcd.euphoria.aura.network",
+    "bip44": {
+      "coinType": 118
+    },
+    "bech32Config": {
+      "bech32PrefixAccAddr": "aura",
+      "bech32PrefixAccPub": "aurapub",
+      "bech32PrefixValAddr": "auravaloper",
+      "bech32PrefixValPub": "auravaloperpub",
+      "bech32PrefixConsAddr": "auravalcons",
+      "bech32PrefixConsPub": "auravalconspub"
+    },
+    "currencies": [
+      {
+        "coinDenom": "EAURA",
+        "coinMinimalDenom": "ueaura",
+        "coinDecimals": 6
+      }
+    ],
+    "feeCurrencies": [
+      {
+        "coinDenom": "EAURA",
+        "coinMinimalDenom": "ueaura",
+        "coinDecimals": 6,
+        "gasPriceStep": {
+          "low": 0.001,
+          "average": 0.0025,
+          "high": 0.004
+        }
+      }
+    ],
+    "stakeCurrency": {
+      "coinDenom": "EAURA",
+      "coinMinimalDenom": "ueaura",
+      "coinDecimals": 6
+    },
+    "coinType": 118,
+    "features": [
+      "ibc-transfer"
+    ],
+    "walletUrlForStaking": "https://euphoria.aurascan.io/validators",
+    "beta": true
+  });
