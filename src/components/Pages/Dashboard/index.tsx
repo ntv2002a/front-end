47a69bc9d90679ca81614e18165268035887c679
Nguyen Trung Vu <trungvu16502@gmail.com>
2023-03-10 13:40:46 +0700

    .socket create room

--- src/components/Pages/Dashboard/index.tsx
+++ src/components/Pages/Dashboard/index.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { ProSidebarProvider } from 'react-pro-sidebar';
 import { SideBar } from '../../Navigation/Sidebar';
 import { GlobalContext, SocketContext } from '../../../App';
-import { RoomSet } from '../../../type';
+import { RoomSet, User } from '../../../type';
 
 
 export const Dashboard = () => {
@@ -23,24 +23,47 @@ export const Dashboard = () => {
                 console.log(roomSet);
             });
 
-            
+
         }
         effectRef.current = false;
     }, [])
 
-    return (
-        <div className='App-dashboard'>
-            <div>
-                <ProSidebarProvider>
-                    <SideBar />
-                </ProSidebarProvider>
-                <div>
-                    <h1 style={{ textAlign: "center" }}>Nội Dung Chính</h1>
-                    <button onClick={handleClick}>Go Back to Home</button>
-                    
+    if (globalContext != null) {
 
+
+        const createRoom = () => {
+            let code: number;
+            code = Math.random() * 100000;
+            let betAmount: number;
+            betAmount = 5;
+            let address: string | undefined;
+            address = globalContext.user?.bech32Address;
+            
+            socket.emit('create new room request', code, betAmount, address);
+        }
+
+        return (
+            <div className='App-dashboard'>
+                <div>
+                    <ProSidebarProvider>
+                        <SideBar />
+                    </ProSidebarProvider>
+                    <div>
+                        <h1 style={{ textAlign: "center" }}>Nội Dung Chính</h1>
+                        <button onClick={handleClick}>Go Back to Home</button>
+                        <button onClick={createRoom}>Create Room</button>
+
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+    else {
+        return (
+            <div>
+                <h1>ERROR!</h1>
+            </div>
+        )
+    }
+
 }
